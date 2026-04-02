df398520d2d4841ee1a4898d799fc6f45350a40b
Adolfo Sabino <adolfosabino@gmail.com>
2026-04-02 09:24:48 +0200

    Sending the foundation files

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'zinnensimulator-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/app.jsx',
+  '/manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone)).catch(() => {});
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
