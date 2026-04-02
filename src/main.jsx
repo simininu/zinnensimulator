ae28fc9d9989781f7504cac54fd496a7d89d0a14
Adolfo Sabino <adolfosabino@gmail.com>
2026-04-02 09:40:52 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NT2Simulator from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <NT2Simulator />
+)
